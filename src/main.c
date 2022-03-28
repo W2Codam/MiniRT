e8ef44e74f5c49685f324f0fc9d84c5a40b636ae
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-03-28 15:27:03 +0200

    Setup initialization and render thread

--- src/main.c
+++ src/main.c
@@ -6,18 +6,57 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/28 11:06:10 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2022/03/28 15:02:04 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/03/28 15:26:20 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MiniRT.h"
 
 /**
+ * Entry routine for the render thread.
+ * 
+ * @param param The game state, rt struct.
+ * @return NULL
+ */
+static void	*ft_render(void *param)
+{
+	(void) param;
+	ft_putendl("Starting rendering...");
+	return (NULL);
+}
+
+/**
+ * Initializes the game state.
+ * 
+ * @param rt The game state to initialize.
+ * @return True or false if Initialion succeeded.
+ */
+static bool	ft_init_rt(t_rt *rt)
+{
+	rt->mlx = mlx_init(STD_WIDTH, STD_HEIGHT, "👾 MegaRT 👾", true);
+	if (mlx_errno)
+	{
+		ft_putendl_fd(mlx_strerror(mlx_errno), STDERR_FILENO);
+		return (false);
+	}
+	rt->window_img = mlx_new_image(rt->mlx, STD_WIDTH, STD_HEIGHT);
+	if (mlx_errno)
+	{
+		ft_putendl_fd(mlx_strerror(mlx_errno), STDERR_FILENO);
+		return (false);
+	}
+	if (pthread_create(&rt->render_thread, NULL, &ft_render, &rt) != 0)
+		return (false);
+	pthread_detach(rt->render_thread);
+	return (true);
+}
+
+/**
  * Hook function to intercept attempt to close the window.
  * 
  * @param param The game state, rt struct.
  */
-void	ft_close_hook(void *param)
+static void	ft_close_hook(void *param)
 {
 	const t_rt	*rt = param;
 
@@ -29,7 +68,7 @@ void	ft_close_hook(void *param)
  * 
  * @param param The game state, rt struct.
  */
-void	ft_hook(void *param)
+static void	ft_hook(void *param)
 {
 	const t_rt	*rt = param;
 
@@ -40,6 +79,9 @@ void	ft_hook(void *param)
 /**
  * Entry point of the application.
  * 
+ * Initilizes the render state as well as launching
+ * a render thread and MLX42.
+ * 
  * @param[in] argc The argument count.
  * @param[in] argv The argument values
  * @return Either EXIT_SUCCESS or EXIT_FAILURE.
@@ -48,23 +90,16 @@ int32_t	main(int32_t argc, char	*argv[])
 {
 	t_rt	rt;
 
+	(void) argv;
 	if (argc - 1 != 1)
 	{
 		ft_putendl_fd("MegaRT: Invalid args: ./MegaRT <file>", STDERR_FILENO);
 		return (EXIT_FAILURE);
 	}
-	rt.mlx = mlx_init(STD_WIDTH, STD_HEIGHT, "👾 MegaRT 👾", true);
-	if (mlx_errno)
-	{
-		ft_putendl_fd(mlx_strerror(mlx_errno), STDERR_FILENO);
+
+	if (!ft_init_rt(&rt))
 		return (EXIT_FAILURE);
-	}
-	rt.window_img = mlx_new_image(rt.mlx, STD_WIDTH, STD_HEIGHT);
-	if (mlx_errno)
-	{
-		ft_putendl_fd(mlx_strerror(mlx_errno), STDERR_FILENO);
-		return (EXIT_FAILURE);
-	}
+
 	mlx_loop_hook(rt.mlx, &ft_hook, &rt);
 	mlx_close_hook(rt.mlx, &ft_close_hook, &rt);
 	mlx_loop(rt.mlx);
