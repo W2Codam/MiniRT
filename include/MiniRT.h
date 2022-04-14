64173edea743a4c0c92c7c0969320ebecfd96bcd
W2.Wizard <63303990+W2Wizard@users.noreply.github.com>
2022-04-14 14:37:21 +0200

    Reduce sample count

--- include/MiniRT.h
+++ include/MiniRT.h
@@ -6,7 +6,7 @@
 /*   By: lde-la-h <lde-la-h@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/11 17:45:39 by lde-la-h      #+#    #+#                 */
-/*   Updated: 2022/04/14 13:28:43 by lde-la-h      ########   odam.nl         */
+/*   Updated: 2022/04/14 13:41:07 by lde-la-h      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@
 # define MAX_LIGHTS		100
 # define MAX_CAMERAS	10
 # define WIN_WIDTH		800
-# define SAMPLE_COUNT	42
+# define SAMPLE_COUNT	10
 
 // Types of entities that exist.
 typedef enum e_EntityType
